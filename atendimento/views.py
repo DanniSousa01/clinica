c48a7c46a1c4a31a1ad707c9a6426aba7c799161
Aluno <you@example.com>
2018-11-30 00:01:03 -0300

    Criação  Detalhes do cliente, onde irão aparecer as todas as informações dos clientes cadastrados.

--- atendimento/views.py
+++ atendimento/views.py
@@ -10,6 +10,10 @@ def cliente_list (request):
     clientes = Cliente.objects.all()
     return render(request, 'cliente/list.html', {'clientes':clientes})
 
+def cliente_show(request, cliente_id):
+    cliente = Cliente.objects.get(pk=cliente_id)
+    return render(request, 'cliente/show.html', {'cliente': cliente})
+
 def medico_list (request):
     medicos = Medico.objects.all()
     return render(request, 'medico/list.html', {'medicos':medicos}) 
